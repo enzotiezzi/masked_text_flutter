febb983eb39970a17014542f4cc005b215bf55b1
enzo tiezzi <enzo_tiezzi@hotmail.com>
2018-05-01 13:17:03 -0300

    refactoring with good practices and fixing issue 1

--- lib/masked_text.dart
+++ lib/masked_text.dart
@@ -3,86 +3,65 @@ library masked_text;
 import 'package:flutter/material.dart';
 
 class MaskedTextField extends StatefulWidget {
-  TextEditingController maskedTextFieldController;
+  final TextEditingController maskedTextFieldController;
 
-  String mask;
-  String escapeCharacter = 'x';
+  final String mask;
+  final String escapeCharacter;
 
-  int maxLength = 100;
-  TextInputType keyboardType = TextInputType.text;
-  InputDecoration inputDecoration = new InputDecoration();
+  final int maxLength;
+  final TextInputType keyboardType;
+  final InputDecoration inputDecoration;
 
-  MaskedTextField(
+  const MaskedTextField(
       {this.mask,
-      this.escapeCharacter,
-      this.maskedTextFieldController,
-      this.maxLength,
-      this.keyboardType,
-      this.inputDecoration});
+        this.escapeCharacter: "x",
+        this.maskedTextFieldController,
+        this.maxLength: 100,
+        this.keyboardType: TextInputType.text,
+        this.inputDecoration: const InputDecoration()});
 
   @override
-  State<StatefulWidget> createState() => new _MaskedTextFieldState(
-      mask: this.mask,
-      maskedTextFieldController: this.maskedTextFieldController,
-      maxLength: this.maxLength,
-      keyboardType: this.keyboardType,
-      inputDecoration: this.inputDecoration);
+  State<StatefulWidget> createState() => new _MaskedTextFieldState();
 }
 
 class _MaskedTextFieldState extends State<MaskedTextField> {
-  TextEditingController maskedTextFieldController;
-
-  String mask;
-  String escapeCharacter;
-
-  int maxLength;
-  TextInputType keyboardType;
-  InputDecoration inputDecoration;
-
-  _MaskedTextFieldState(
-      {this.mask,
-      this.escapeCharacter,
-      this.maskedTextFieldController,
-      this.maxLength,
-      this.keyboardType,
-      this.inputDecoration});
-
   @override
   Widget build(BuildContext context) {
     var lastTextSize = 0;
 
     return new TextField(
-      controller: maskedTextFieldController,
-      maxLength: maxLength,
-      keyboardType: keyboardType,
-      decoration: inputDecoration,
+      controller: widget.maskedTextFieldController,
+      maxLength: widget.maxLength,
+      keyboardType: widget.keyboardType,
+      decoration: widget.inputDecoration,
       onChanged: (String text) {
         // its deleting text
         if (text.length < lastTextSize) {
-          if (mask[text.length] != escapeCharacter)
-            maskedTextFieldController.text = text.substring(0, text.length - 1);
-        }
+          if (widget.mask[text.length] != widget.escapeCharacter)
+            widget.maskedTextFieldController.text = text.substring(0, text.length - 1);
+        } else {
+          // its typing
+          if (text.length >= lastTextSize) {
+            var position = text.length;
 
-        // its typing
-        if (text.length >= lastTextSize) {
-          var position = text.length;
+            if (widget.mask[position] != widget.escapeCharacter)
+              widget.maskedTextFieldController.text =
+              "${widget.maskedTextFieldController.text}${widget.mask[position]}";
+          }
 
-          if (mask[position] != escapeCharacter)
-            maskedTextFieldController.text =
-                "${maskedTextFieldController.text}${mask[position]}";
-        }
-
-        // Android on change resets cursor position(cursor goes to 0)
-        // so you have to check if it reseted, then put in the end(just on android)
-        // as IOS bugs if you simply put it in the end
-        if (maskedTextFieldController.selection.start <
-            maskedTextFieldController.text.length) {
-          maskedTextFieldController.selection = new TextSelection.fromPosition(
-              new TextPosition(offset: maskedTextFieldController.text.length));
+          // Android on change resets cursor position(cursor goes to 0)
+          // so you have to check if it reset, then put in the end(just on android)
+          // as IOS bugs if you simply put it in the end
+          if (widget.maskedTextFieldController.selection.start <
+              widget.maskedTextFieldController.text.length) {
+            widget.maskedTextFieldController.selection =
+            new TextSelection.fromPosition(new TextPosition(
+                offset: widget.maskedTextFieldController.text.length));
+          }
         }
 
         // update cursor position
-        lastTextSize = text.length;
+        lastTextSize = widget.maskedTextFieldController.text.length;
       },
     );
   }
